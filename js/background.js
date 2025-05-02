d9b029cd5a15bb648139af3f9e9e180767c592ce
Yubeen Lee <lybin71@naver.com>
2025-05-02 19:54:20 +0100

    feat: add bg img

---
+++ js/background.js
@@ -0,0 +1,6 @@
+const imgs = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+const randomImg = imgs[Math.floor(Math.random() * imgs.length)];
+
+const bgImg = document.createElement("img");
+bgImg.src = `img/${randomImg}`;
+document.body.appendChild(bgImg);
